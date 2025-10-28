376031de7cc6ef81d0acc22942f0f19284748b23
taylora23 <aisha.taylor@ambitionspersonnel.com>
2025-10-28 10:14:20 +0000

    Add files via upload

---
+++ AGH Calculator.py
@@ -0,0 +1,32 @@
+import streamlit as st
+
+st.set_page_config(page_title="AGH Volume-Driven Annual Savings Calculator", layout="wide")
+
+st.image("Ambitions Public Sector Logo.png", width=240)
+st.markdown(
+    "<h1 style='font-size: 40px;'>AGH Volume-Driven Annual Savings Calculator</h1>",
+    unsafe_allow_html=True
+)
+
+fixed_number = 15941.60
+
+st.markdown(
+    '<p style="font-size:25px; margin: 0; line-height: 1.1;">Enter the quantity of workers:</p>',
+    unsafe_allow_html=True
+)
+
+user_input = st.number_input(
+  "", min_value=1, step=1, format="%d")
+
+result = user_input * fixed_number
+
+formatted_result = f"£{result:,.2f}"
+
+st.markdown(
+    f"""
+    <p style='color:red; font-size:40px; font-weight:bold; text-align:center;'>
+        Annual Savings Achieved: {formatted_result}
+    </p>
+    """,
+    unsafe_allow_html=True
+)
